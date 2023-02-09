bf01d630af228977d49e583e460b2dc64201e8b9
Desmond O'Leary <desoleary@gmail.com>
2023-02-08 18:12:45 -0700

    `error_info.rb` ~ Helps summarize errors to aid in debugging errors

---
+++ lib/light_service_ext/error_info.rb
@@ -0,0 +1,69 @@
+# frozen_string_literal: true
+
+module LightServiceExt
+  class ErrorInfo
+    attr_reader :error, :type, :message, :title, :ctx
+
+    def initialize(error, ctx: nil, message: nil, fatal: false)
+      @fatal = fatal
+      @error = error
+      @type = error.class.name
+      @message = message || error.message
+      @title = "#{error.class.name} : #{error.message}"
+      @ctx = ctx
+    end
+
+    def fatal_error?
+      @fatal || !non_fatal_error?
+    end
+
+    def error_summary
+      header = fatal_error? ? "SERVER ERROR FOUND" : "ERROR FOUND"
+
+      <<~TEXT
+        =========== #{header}: #{title} ===========\n
+        #{clean_backtrace[0, 3]&.join("\n")}
+        #{'=' * 56}
+
+        FULL STACK TRACE
+        #{clean_backtrace.join("\n")}
+
+        #{'=' * 56}
+      TEXT
+    end
+
+    def to_h
+      {
+        type: type,
+        message: message,
+        exception: title,
+        backtrace: clean_backtrace[0, 3]&.join("\n"),
+        ctx: ctx&.to_h,
+        error: exception,
+        fatal_error?: fatal_error?
+      }
+    end
+
+    def clean_backtrace
+      @clean_backtrace ||= begin
+                             return error.backtrace || [] unless defined? Rails
+
+                             Rails.backtrace_cleaner.clean(error.backtrace || [])
+                           end
+    end
+
+    def non_fatal_error?
+      self.class.non_fatal_errors.map(&:to_s).include?(type)
+    end
+
+    private
+
+    class << self
+      attr_writer :non_fatal_errors
+
+      def non_fatal_errors
+        @non_fatal_errors ||= []
+      end
+    end
+  end
+end
---
+++ spec/light_service_ext/error_info_spec.rb
@@ -0,0 +1,89 @@
+RSpec.describe LightServiceExt::ErrorInfo do
+  let(:value) { 'some-value' }
+  let(:ctx) { { key: value } }
+  let(:message_override) { nil }
+  let(:message) { 'some-error' }
+  let(:fatal) { nil }
+  let(:backtrace) { ['some-backtrace-item'] }
+  let(:error) { StandardError.new(message) }
+  let(:error_info_class) { Class.new(described_class) }
+
+  subject(:instance) { error_info_class.new(error, ctx: ctx, message: message_override, fatal: fatal) }
+
+  before(:each) do
+    error.set_backtrace(backtrace)
+  end
+
+  describe '#type' do
+    it 'returns error class name' do
+      expect(instance.type).to eql(error.class.name)
+    end
+  end
+
+  describe '#title' do
+    it 'returns error title with class name and message' do
+      expect(instance.title).to eql("#{error.class.name} : #{message}")
+    end
+  end
+
+  describe 'message' do
+    it 'returns error message' do
+      expect(instance.message).to eql(message)
+    end
+
+    context 'with message override' do
+      let(:message_override) { 'some-other-message' }
+
+      it 'returns error message override' do
+        expect(instance.message).to eql(message_override)
+      end
+    end
+  end
+
+  describe '#error' do
+    it 'returns original exception' do
+      expect(instance.error).to eql(error)
+    end
+  end
+
+  describe '#fatal_error?' do
+    it 'returns true' do
+      expect(instance.fatal_error?).to be_truthy
+    end
+
+    context 'with non fatal error' do
+      let(:error) { ArgumentError.new(message) }
+
+      before(:each) { error_info_class.non_fatal_errors = [ArgumentError] }
+
+      context 'with fatal passed as arg' do
+        let(:fatal) { true }
+
+        it 'returns true' do
+          expect(instance.fatal_error?).to be_truthy
+        end
+      end
+
+      it 'returns false' do
+        expect(instance.fatal_error?).to be_falsey
+      end
+    end
+  end
+
+  describe '#error_summary' do
+    it 'returns summary of error' do
+      expect(instance.error_summary).to eql(<<-TEXT
+=========== SERVER ERROR FOUND: StandardError : some-error ===========
+
+some-backtrace-item
+========================================================
+
+FULL STACK TRACE
+some-backtrace-item
+
+========================================================
+TEXT
+)
+    end
+  end
+end
