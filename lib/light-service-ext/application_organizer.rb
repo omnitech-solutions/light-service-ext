a326d21fd671924ba10633c88a060756f41ffcd3
Desmond O'Leary <desoleary@gmail.com>
2023-02-24 14:34:30 -0700

    `application_organizer.rb` ~ hook up `RecordActions` and `AllActionsCompleteAction`

--- lib/light-service-ext/application_organizer.rb
+++ lib/light-service-ext/application_organizer.rb
@@ -5,11 +5,19 @@ module LightServiceExt
     extend LightService::Organizer
 
     def self.call(context)
-      with(ApplicationContext.make_with_defaults(context)).reduce(steps)
+      with(ApplicationContext.make_with_defaults(context))
+        .around_each(RecordActions)
+        .reduce(all_steps)
     end
 
     def self.steps
       raise NotImplementedError
+    end
+
+    def self.all_steps
+      return steps.push(AllActionsCompleteAction) if steps.is_a?(Array)
+
+      [steps].push(AllActionsCompleteAction)
     end
   end
 end
